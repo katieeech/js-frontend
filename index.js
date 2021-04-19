a3c1906189fe04cba5cafc52d61c9c10a765184e
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-19 17:49:08 -0400

    Committing along with backend changes on creating reviews

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", (e) =>{
+document.addEventListener("DOMContentLoaded", (e) => {
     //stable elements
     let cafeName = document.querySelector(".cafe-name")
     let cafeAddress = document.querySelector(".cafe-address")
     let cafeImgContainer = document.querySelector(".cafe-image-container")
     let cafeAvgHeartContainer = document.querySelector(".avg-cafe-hearts")
     let leaveReviewForm = document.querySelector(".review-form")
+    let reviewTextarea = document.querySelector(".review-form > textarea")
     let cafeReviewUL = document.querySelector(".reviews")
     let displayedCafe = {}
+
 
     fetch("http://localhost:3000/cafes/1")
         .then(res => res.json())
@@ -34,23 +36,55 @@ document.addEventListener("DOMContentLoaded", (e) =>{
             })
         })
 
+
+
     leaveReviewForm.addEventListener("submit", (evt) => {
         evt.preventDefault()
-        let userReviewInput = evt.target.reviewInput.value
+        let userReviewInputString = reviewTextarea.value
+
         let newReviewArr = displayedCafe.reviews
-        newReviewArr.push(userReviewInput)
-        fetch("http://localhost:3000/cafes", {
-            method: "POST",
+
+        let lastObj = displayedCafe.reviews[displayedCafe.reviews.length - 1]
+
+        let lastId = lastObj.userId
+
+
+
+
+        let userReviewInputObj = {
+            userId: lastId + 1,
+            userReview: userReviewInputString,
+            userRating: 5,
+            userImage: "https://s3-media0.fl.yelpcdn.com/bphoto/LyX3SP8acoujEDrTQcsa6Q/300s.jpg"
+        }
+
+        newReviewArr.push(userReviewInputObj)
+
+        fetch("http://localhost:3000/cafes/1", {
+            method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                reviews: newReviewArr 
+                reviews: newReviewArr
             })
         })
             .then(resp => resp.json())
             .then((updatedCafeObj) => {
-                console.log(updatedCafeObj.reviews)
+                // Update the DOM
+
+                let cafeReviewLI = document.createElement("li")
+                let reviewText = document.createElement("p")
+                reviewText.innerText = userReviewInputString
+                let reviewHeartRating = document.createElement("span")
+                reviewHeartRating.innerText = updatedCafeObj.reviews[updatedCafeObj.reviews.length - 1].userRating
+                let reviewImg = document.createElement("img")
+                reviewImg.src = updatedCafeObj.reviews[updatedCafeObj.reviews.length - 1].userImage
+                cafeReviewLI.append(reviewText, reviewHeartRating, reviewImg)
+                cafeReviewUL.append(cafeReviewLI)
+
+                //Update the Object in Memory
+                displayedCafe = updatedCafeObj
             })
     })
 
