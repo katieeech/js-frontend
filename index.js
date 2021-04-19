31a2371a901049aa1f310ffba6c4c5f43c297ebf
Leonides <xxluckyleoxx1@gmail.com>
2021-04-19 16:41:20 -0400

    started adding review

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", (e) =>{
     let cafeAvgHeartContainer = document.querySelector(".avg-cafe-hearts")
     let leaveReviewForm = document.querySelector(".review-form")
     let cafeReviewUL = document.querySelector(".reviews")
+    let displayedCafe = {}
 
     fetch("http://localhost:3000/cafes/1")
         .then(res => res.json())
         .then((cafeObj) => {
+            displayedCafe = cafeObj
             cafeName.innerText = cafeObj.name
             cafeAddress.innerText = cafeObj.address
             cafeImgContainer.innerHTML = ''
@@ -31,5 +33,25 @@ document.addEventListener("DOMContentLoaded", (e) =>{
                 cafeReviewUL.append(cafeReviewLI)
             })
         })
+
+    leaveReviewForm.addEventListener("submit", (evt) => {
+        evt.preventDefault()
+        let userReviewInput = evt.target.reviewInput.value
+        let newReviewArr = displayedCafe.reviews
+        newReviewArr.push(userReviewInput)
+        fetch("http://localhost:3000/cafes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                reviews: newReviewArr 
+            })
+        })
+            .then(resp => resp.json())
+            .then((updatedCafeObj) => {
+                console.log(updatedCafeObj.reviews)
+            })
+    })
 
 })
