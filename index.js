3df6e4d1eaf5d675be8926681f271c22dde2df7e
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-20 14:13:35 -0400

    Finished making an edit review button

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     let displayedCafe = {}
 
 
+    //Make a GET fetch
     fetch("http://localhost:3000/cafes/1")
         .then(res => res.json())
         .then((cafeObj) => {
@@ -37,7 +38,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
         })
 
 
-
+    //Adding an Event Listener on leaveReviewForm
     leaveReviewForm.addEventListener("submit", (evt) => {
         evt.preventDefault()
         let userReviewInputString = reviewTextarea.value
@@ -48,18 +49,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
         let lastId = lastObj.userId
 
-
-
-
         let userReviewInputObj = {
             userId: lastId + 1,
             userReview: userReviewInputString,
             userRating: 5,
-            userImage: "https://s3-media0.fl.yelpcdn.com/bphoto/LyX3SP8acoujEDrTQcsa6Q/300s.jpg"
+            userImage: "https://s3-media0.fl.yelpcdn.com/bphoto/yF67XONxES0imLkXaqJnGg/o.jpg"
         }
 
         newReviewArr.push(userReviewInputObj)
 
+        //Make a PATCH fetch request
         fetch("http://localhost:3000/cafes/1", {
             method: "PATCH",
             headers: {
@@ -80,12 +79,70 @@ document.addEventListener("DOMContentLoaded", (e) => {
                 reviewHeartRating.innerText = updatedCafeObj.reviews[updatedCafeObj.reviews.length - 1].userRating
                 let reviewImg = document.createElement("img")
                 reviewImg.src = updatedCafeObj.reviews[updatedCafeObj.reviews.length - 1].userImage
-                cafeReviewLI.append(reviewText, reviewHeartRating, reviewImg)
+                let reviewEditBtn = document.createElement("button")
+                reviewEditBtn.innerText = "Edit"
+                cafeReviewLI.append(reviewText, reviewHeartRating, reviewImg, reviewEditBtn)
                 cafeReviewUL.append(cafeReviewLI)
 
                 //Update the Object in Memory
                 displayedCafe = updatedCafeObj
+
+                //Adding an Event Listener on reviewEditBtn
+                reviewEditBtn.addEventListener("click", (evt) => {
+                    let reviewNewEditForm = document.createElement("form")
+
+                    let reviewNewTextarea = document.createElement("textarea")
+                    reviewNewTextarea.innerText = "Edit your review here!"
+                    let reviewNewSubmitInput = document.createElement("input")
+                    reviewNewSubmitInput.type = "submit"
+                    reviewNewSubmitInput.value = "Submit"
+                    reviewNewSubmitInput.name = "newReviewInput"
+
+                    reviewNewEditForm.append(reviewNewTextarea, reviewNewSubmitInput)
+                    cafeReviewLI.append(reviewNewEditForm)
+
+
+                    //Adding an Event Listener on reviewEditForm (submit button)
+                    reviewNewEditForm.addEventListener("submit", (evt) => {
+                        evt.preventDefault()
+
+                        let userReviewNewInputString = reviewNewTextarea.value
+                        let editedNewReview = displayedCafe.reviews[displayedCafe.reviews.length - 1].userReview
+                        editedNewReview = userReviewNewInputString
+                        let originalReviewArr = displayedCafe.reviews
+
+                        let userReviewInputObj = {
+                            userId: displayedCafe.reviews[displayedCafe.reviews.length - 1].userId,
+                            userReview: editedNewReview,
+                            userRating: displayedCafe.reviews[displayedCafe.reviews.length - 1].userRating,
+                            userImage: displayedCafe.reviews[displayedCafe.reviews.length - 1].userImage
+                        }
+
+                        originalReviewArr[originalReviewArr.length - 1] = userReviewInputObj
+
+                        fetch("http://localhost:3000/cafes/1", {
+                            method: "PATCH",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                reviews: originalReviewArr
+                            })
+                        })
+                            .then(resp => resp.json())
+                            .then((updatedCafeWithReview) => {
+                                //Update the DOM
+                                reviewText.innerText = updatedCafeWithReview.reviews[updatedCafeWithReview.reviews.length - 1].userReview
+                                //Update the Object in Memory
+                                displayedCafe = updatedCafeWithReview
+                            })
+                        evt.target.reset()
+                    })
+
+                })
             })
+
     })
+
 
 })
